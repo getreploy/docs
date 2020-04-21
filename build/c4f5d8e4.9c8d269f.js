028a02104736423a2473cf98275573f584a4f640
Himank Yadav <him229@gmail.com>
2020-04-20 17:33:53 -0700

    add build

---
+++ build/c4f5d8e4.9c8d269f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{108:function(e,n,t){"use strict";t.r(n);var c=t(0),a=t.n(c),o=t(113),r=t(115),s=t(2);n.default=function(){return Object(o.a)(),a.a.createElement(s.a,{to:Object(r.a)("docs/doc1")})}}}]);
